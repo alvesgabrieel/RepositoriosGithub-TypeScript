958f4fd40abcc66d28a77d84609f5991d2396030
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-05-16 13:31:55 -0300

    building Project section

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,7 +1,39 @@
 import Titulo from '../../components/Titulo'
+import Projeto from '../../components/Projeto'
+import { Lista } from './styles'
 
 const Projetos = () => {
-  return <Titulo fontSize={16}>Projeto</Titulo>
+  return (
+    <section>
+      <Titulo fontSize={16}>Projeto</Titulo>
+      <Lista>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+        <li>
+          <Projeto></Projeto>
+        </li>
+      </Lista>
+    </section>
+  )
 }
 
 export default Projetos
